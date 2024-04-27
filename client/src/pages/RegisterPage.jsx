636322f267cc9a185410f299350d245009ea70b8
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-04-27 12:36:57 +1000

    add register page and login page and add api to them

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const RegisterPage = () => {
+  const [name, setname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function registerUser(ev) {
+    ev.preventDefault();
+    axios.get("http://localhost:4000/test");
+  }
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-32">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form onSubmit={registerUser} className="max-w-md mx-auto ">
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(event) => setname(event.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="paswword"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button className="primary">Register</button>
+          <div className="text-center py-2 text-gray-500">
+            Alreadt a member?{" "}
+            <Link className="underline text-black" to={"/register"}>
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
