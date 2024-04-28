f496ab3f8b7db76998063b91cee8c2bfbebc9a3d
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-04-28 16:23:34 +1000

    add login page with token

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User= require('./models/User.js');
 require('dotenv').config();
 const app = express();
@@ -13,7 +14,7 @@ const bcryptSalt = bcrypt.genSaltSync(10)
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:5175'
+    origin:'http://localhost:5173'
 
 }));
   mongoose.connect(process.env.MONGO_URL)
@@ -36,5 +37,34 @@ app.get('/test',(req,res)=>{
                 }
     
 })
+
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body
+    
+        const userFind = await User.findOne({email})
+        if(userFind){
+            res.json('userfind')
+            const passOk= bcrypt.compareSync(password,userFind.password)
+            if(passOk){
+                res.cookie('token', '').json('passok')
+            }
+            else{
+                res.status(422).json('passnotok')
+            }
+        }
+        else{
+            res.json('usernotfound')
+          
+          
+          
+          
+          
+          
+   
+   
+   
+   
+
+        }})
 
 app.listen(4000)
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,45 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function loginUser(ev) {
+    ev.preventDefault();
+    try {
+      await axios.post("/login", {
+        email,
+        password,
+      });
+      alert("login successful");
+    } catch (e) {
+      alert("login failed");
+    }
+  }
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto ">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="paswword" />
+        <form onSubmit={loginUser} className="max-w-md mx-auto ">
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="paswword"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
           <button className="primary">Login</button>
           <div className="text-center py-2 text-gray-500">
             Don't have an account yet?{" "}
